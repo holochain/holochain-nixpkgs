deceb5478fb8d669866d02b79efb5b14aad5c9af
Stefan Junker <mail@stefanjunker.de>
2022-01-03 21:17:26 +0100

    incomplete: stub out running the crate tests in nix

--- packages/default.nix
+++ packages/default.nix
@@ -10,9 +10,21 @@ let
   update-holochain-versions = pkgs.buildEnv {
     name = "update-holochain-versions";
     paths = [
-      crate2nixGenerated
-        .workspaceMembers.update-holochain-versions
-        .build
+      (crate2nixGenerated.workspaceMembers.update-holochain-versions.build.override {
+        # TODO: tests run nix which currently fails within a nix build.
+        runTests = false;
+        testPreRun = ''
+          mv test test.bkp
+          mkdir test
+          ${pkgs.rsync}/bin/rsync -rLv test.bkp/ test/
+          find test/
+          chmod -R +w test
+
+          # mkdir nix-store
+          export NIX_PATH=nixpkgs=${pkgs.path}
+        '';
+        testInputs = [ pkgs.nixUnstable ];
+      })
       pkgs.nvfetcher
     ];
   };
