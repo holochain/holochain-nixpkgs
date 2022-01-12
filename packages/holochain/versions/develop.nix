29b44fd336f08fa5541358ca9aad994c045ea5af
Jost Schulte <jost.schulte@protonmail.com>
2022-01-12 14:07:45 +0100

    update nvfetcher state

--- packages/holochain/versions/develop.nix
+++ packages/holochain/versions/develop.nix
@@ -4,8 +4,8 @@
 
 {
     url = "https://github.com/holochain/holochain";
-    rev = "f4b3a94d027a73a693eef511c8a5177373a04364";
-    sha256 = "sha256-4MpKK7QqqqFsomHh/XVtYMyoA3AnB45qVDhdh4H5ngA=";
+    rev = "24f089707b39a31780f6b86fbdc4859113374a23";
+    sha256 = "sha256-77HZm3yn40TNbuva7S+fuHdxrzr/NgIquFvgtdrLqQk=";
     cargoLock = {
         outputHashes = {
             "cargo-test-macro-0.1.0" = "sha256-hIGpT0n41CA24vss4itXS3O2XrznsBce/60PUVrwwfs=";
