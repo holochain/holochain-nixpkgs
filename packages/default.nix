5a4a5d799a1e7be7143937e23ccba6ac35cb6c45
Stefan Junker <mail@stefanjunker.de>
2022-02-25 16:46:46 +0100

    WIP: add util and autoupdate job

--- packages/default.nix
+++ packages/default.nix
@@ -1,5 +1,6 @@
 { pkgs
 , lib
+, stdenv
 , callPackage
 , symlinkJoin
 , nvfetcher
@@ -7,12 +8,59 @@
 , makeWrapper
 , mkRust
 , makeRustPlatform
+, darwin
+, xcbuild
 }:
 
 let
+  rustInputAttrs = attrs: {
+    OPENSSL_NO_VENDOR = "1";
+    OPENSSL_LIB_DIR = "${opensslStatic.out}/lib";
+    OPENSSL_INCLUDE_DIR = "${opensslStatic.dev}/include";
+
+    nativeBuildInputs = (attrs.nativeBuildInputs or [ ])
+      ++ (with pkgs; [
+      perl
+      pkg-config
+    ])
+      ++ (lib.optionals stdenv.isDarwin [
+      xcbuild
+    ])
+    ;
+
+    buildInputs = (attrs.buildInputs or [ ])
+      ++ (with pkgs; [
+      openssl
+      zlib
+      opensslStatic
+      libgit2
+      libssh2
+    ])
+      ++ (lib.optionals stdenv.isDarwin (with darwin.apple_sdk.frameworks; [
+      AppKit
+      CoreFoundation
+      CoreServices
+      Security
+      libiconv
+    ]))
+    ;
+  };
+  opensslStatic = pkgs.openssl.override (_: {
+    static = true;
+  });
   holochain = callPackage ./holochain { inherit mkRust makeRustPlatform; };
-  crate2nixGenerated = import ../nix/crate2nix/Cargo.nix { inherit pkgs; };
-  crate2nix = crate2nixGenerated.workspaceMembers.update-holochain-versions.build.override {
+  crate2nixGenerated =
+    let
+    in
+    import ../nix/crate2nix/Cargo.nix {
+      inherit pkgs;
+      defaultCrateOverrides = pkgs.defaultCrateOverrides // {
+        openssl-sys = rustInputAttrs;
+        libgit2-sys = rustInputAttrs;
+        libssh2-sys = rustInputAttrs;
+      };
+    };
+  update-holochain-versions-raw = crate2nixGenerated.workspaceMembers.update-holochain-versions.build.override {
     # TODO: tests run nix which currently fails within a nix build.
     runTests = false;
     testPreRun = ''
@@ -28,24 +76,24 @@ let
     testInputs = [ pkgs.nixUnstable ];
   };
   update-holochain-versions = symlinkJoin {
-    inherit (crate2nix) name;
-    paths = [ crate2nix ];
+    inherit (update-holochain-versions-raw) name;
+    paths = [ update-holochain-versions-raw ];
     buildInputs = [ makeWrapper ];
     postBuild = ''
       wrapProgram $out/bin/update-holochain-versions \
             --suffix PATH ":" ${lib.makeBinPath [ nixUnstable nvfetcher ]}
     '';
   };
-in
-{
-  inherit
-    holochain
-    update-holochain-versions
-    ;
-
-  inherit (pkgs)
-    nvfetcher
-    ;
+  holochain-nixpkgs-util-raw = crate2nixGenerated.workspaceMembers.holochain-nixpkgs-util.build;
+  holochain-nixpkgs-util = symlinkJoin {
+    name = holochain-nixpkgs-util-raw.name;
+    paths = [ holochain-nixpkgs-util-raw ];
+    buildInputs = [ makeWrapper ];
+    postBuild = ''
+      wrapProgram $out/bin/holochain-nixpkgs-util \
+            --suffix PATH ":" ${lib.makeBinPath [ scripts.hnixpkgs-update-single ]}
+    '';
+  };
 
   scripts = callPackage ./scripts.nix {
     inherit
@@ -53,4 +101,19 @@ in
       update-holochain-versions
       ;
   };
+in
+{
+  inherit
+    scripts
+    holochain
+    update-holochain-versions
+    holochain-nixpkgs-util
+    rustInputAttrs
+    ;
+
+
+  inherit (pkgs)
+    nvfetcher
+    ;
+
 }
