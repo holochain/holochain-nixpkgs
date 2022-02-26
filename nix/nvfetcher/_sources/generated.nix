6432f0581d1ffa5177fb52037e19a710c3a48bec
Holochain Release Automation <hrabot@holochain.org>
2022-02-26 22:23:38 +0000

    update nvfetcher sources
    the following keys were updated
    
    v0_0_114

--- nix/nvfetcher/_sources/generated.nix
+++ nix/nvfetcher/_sources/generated.nix
@@ -264,6 +264,24 @@
       };
     };
   };
+  holochain_revision_holochain-0_0_114 = {
+    pname = "holochain_revision_holochain-0_0_114";
+    version = "holochain-0.0.114";
+    src = fetchgit {
+      url = "https://github.com/holochain/holochain";
+      rev = "holochain-0.0.114";
+      fetchSubmodules = false;
+      deepClone = false;
+      leaveDotGit = false;
+      sha256 = "sha256-ocPRN1+HyPrp/TtqV0k5xsn2pP9+i4AXM6cYZlntbuQ=";
+    };
+    cargoLock = {
+      lockFile = ./holochain_revision_holochain-0_0_114-holochain-0.0.114/Cargo.lock;
+      outputHashes = {
+        "cargo-test-macro-0.1.0" = "sha256-hIGpT0n41CA24vss4itXS3O2XrznsBce/60PUVrwwfs=";
+      };
+    };
+  };
   holochain_revision_holochain-0_0_115 = {
     pname = "holochain_revision_holochain-0_0_115";
     version = "holochain-0.0.115";
---
+++ packages/holochain/versions/v0_0_114.nix
@@ -0,0 +1,39 @@
+# This file was generated with the following command:
+# update-holochain-versions --nvfetcher-dir=nix/nvfetcher --output-file=packages/holochain/versions/v0_0_114.nix --git-src=revision:holochain-0.0.114 --lair-version-req=~0.0
+# For usage instructions please visit https://github.com/holochain/holochain-nixpkgs/#readme
+
+{
+    url = "https://github.com/holochain/holochain";
+    rev = "holochain-0.0.114";
+    sha256 = "sha256-ocPRN1+HyPrp/TtqV0k5xsn2pP9+i4AXM6cYZlntbuQ=";
+    cargoLock = {
+        outputHashes = {
+            "cargo-test-macro-0.1.0" = "sha256-hIGpT0n41CA24vss4itXS3O2XrznsBce/60PUVrwwfs=";
+        };
+    };
+
+    binsFilter = [
+        "holochain"
+        "hc"
+        "kitsune-p2p-proxy"
+    ];
+
+    rustVersion = "1.58.1";
+
+    lair = {
+        url = "https://github.com/holochain/lair";
+        rev = "v0.0.8";
+        sha256 = "sha256-AeyEQWLP2xw8WezDHqfq0oEFxYWY0E7kmuciz/qtCP8=";
+
+        binsFilter = [
+            "lair-keystore"
+        ];
+
+        rustVersion = "1.58.1";
+
+        cargoLock = {
+            outputHashes = {
+            };
+        };
+    };
+}
