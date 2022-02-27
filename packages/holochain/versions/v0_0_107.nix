c426d652aa96f9ca6de8e05ea50590d1d5646467
Holochain Release Automation <hrabot@holochain.org>
2022-02-27 09:33:19 +0000

    update nvfetcher sources
    the following keys were updated
    
    v0_0_107

---
+++ packages/holochain/versions/v0_0_107.nix
@@ -0,0 +1,39 @@
+# This file was generated with the following command:
+# update-holochain-versions --nvfetcher-dir=nix/nvfetcher --output-file=packages/holochain/versions/v0_0_107.nix --git-src=revision:holochain-0.0.107 --lair-version-req=~0.0
+# For usage instructions please visit https://github.com/holochain/holochain-nixpkgs/#readme
+
+{
+    url = "https://github.com/holochain/holochain";
+    rev = "holochain-0.0.107";
+    sha256 = "sha256-ccZyr82y//UJUzospykYbdK1C8OyL7+aVCTfDiCkrNQ=";
+    cargoLock = {
+        outputHashes = {
+            "cargo-test-macro-0.1.0" = "sha256-hIGpT0n41CA24vss4itXS3O2XrznsBce/60PUVrwwfs=";
+        };
+    };
+
+    binsFilter = [
+        "holochain"
+        "hc"
+        "kitsune-p2p-proxy"
+    ];
+
+    rustVersion = "1.58.1";
+
+    lair = {
+        url = "https://github.com/holochain/lair";
+        rev = "v0.0.4";
+        sha256 = "sha256-2zPhOSHH8mX6F3kfymNf9p+YxdLf4y3E0+G25wovD04=";
+
+        binsFilter = [
+            "lair-keystore"
+        ];
+
+        rustVersion = "1.58.1";
+
+        cargoLock = {
+            outputHashes = {
+            };
+        };
+    };
+}
