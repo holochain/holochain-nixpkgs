780b62c0e693043a4c2550ac854b77b636d096ee
Holochain Release Automation <hrabot@holochain.org>
2022-04-21 19:58:58 +0000

    update nvfetcher source: nixpkgs

--- nix/nvfetcher/_sources/generated.nix
+++ nix/nvfetcher/_sources/generated.nix
@@ -716,10 +716,10 @@
   };
   nixpkgs = {
     pname = "nixpkgs";
-    version = "9887f024766aa27704d1f89f623efd1d063da92a";
+    version = "f0d8b069143f12ca41174bc80a5bc03cca7be438";
     src = fetchTarball {
-      url = "https://github.com/nixos/nixpkgs/archive/9887f024766aa27704d1f89f623efd1d063da92a.tar.gz";
-      sha256 = "07fgr087sir8ra1v9icp2011f1s2iqlclyinxy0jrlawyz9rz4h2";
+      url = "https://github.com/nixos/nixpkgs/archive/f0d8b069143f12ca41174bc80a5bc03cca7be438.tar.gz";
+      sha256 = "1nq6cp6sqvnf03nnh1s0nimd1qblkm95q5z1vmw4627hfp09c2pb";
     };
   };
   nvfetcher = {
