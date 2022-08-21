d975e884c899aff87a9f077de5771481daf479ca
Holochain Release Automation <100725712+holochain-release-automation2@users.noreply.github.com>
2022-08-21 16:59:51 +0200

    update nvfetcher sources (#603)
    the following keys were updated
    
    main
    
    Co-authored-by: Holochain Release Automation <hrabot@holochain.org>

--- packages/holochain/versions/main.nix
+++ packages/holochain/versions/main.nix
@@ -1,11 +1,11 @@
 # This file was generated with the following command:
-# update-holochain-versions --nvfetcher-dir=nix/nvfetcher --output-file=packages/holochain/versions/main.nix --bins-filter=holochain,hc,kitsune-p2p-proxy,kitsune-p2p-tx2-proxy --git-src=branch:main --lair-version-req=~0.0
+# update-holochain-versions --nvfetcher-dir=nix/nvfetcher --output-file=packages/holochain/versions/main.nix --bins-filter=holochain,hc,kitsune-p2p-proxy,kitsune-p2p-tx2-proxy --git-src=branch:main --lair-version-req=~0.2
 # For usage instructions please visit https://github.com/holochain/holochain-nixpkgs/#readme
 
 {
     url = "https://github.com/holochain/holochain";
-    rev = "695e6f9571550c351900e778a793205f13298c8c";
-    sha256 = "sha256-ohVK98M3XuVu5R2y1ZyakAa+Wacru0LNC7PlPhMi5sQ=";
+    rev = "ab8c5552111da73971554ea3d80e473c97b5c650";
+    sha256 = "sha256-SmX+JNJl2NuORzaF31l4m3fcq0CHrIDIdQjFbbf5Ado=";
     cargoLock = {
         outputHashes = {
         };
@@ -21,8 +21,8 @@
 
     lair = {
         url = "https://github.com/holochain/lair";
-        rev = "v0.0.11";
-        sha256 = "sha256-ZmRBVu2dlgLT/Kr/4PoVf4rD1jzexXyWU+dggrv7IrQ=";
+        rev = "lair_keystore_api-v0.2.0";
+        sha256 = "sha256-n7nZyZR0Q68Uff7bTSVFtSDLi21CNcyKibOBx55Gasg=";
 
         binsFilter = [
             "lair-keystore"
