f34da4edade1b183fd7fda5a4f2ffce12457c71c
Holochain Release Automation <100725712+holochain-release-automation2@users.noreply.github.com>
2022-09-07 08:43:48 +0200

    update holochain-tags on develop (#638)
    * update holochain tags
    
    packages/holochain/versions/update_config.toml:
    
    changed: true
    added (1): {
        "v0_0_159",
    }
    
    .github/workflows/build.yml:
    
    changed: true
    removed (1): {
        "v0_0_152",
    }
    added (1): {
        "v0_0_159",
    }
    
    * update nvfetcher sources
    
    the following keys were updated
    
    v0_0_159
    
    Co-authored-by: Holochain Release Automation <hrabot@holochain.org>

---
+++ packages/holochain/versions/v0_0_159.nix
@@ -0,0 +1,37 @@
+# This file was generated with the following command:
+# update-holochain-versions --nvfetcher-dir=nix/nvfetcher --output-file=packages/holochain/versions/v0_0_159.nix --bins-filter=holochain,hc,kitsune-p2p-proxy,kitsune-p2p-tx2-proxy --git-src=revision:holochain-0.0.159 --lair-version-req=~0.2
+# For usage instructions please visit https://github.com/holochain/holochain-nixpkgs/#readme
+
+{
+    url = "https://github.com/holochain/holochain";
+    rev = "holochain-0.0.159";
+    sha256 = "sha256-mpL1NbLquzwttOFY6xPfg1PPEOcMZcSbamwIXx8iIrY=";
+    cargoLock = {
+        outputHashes = {
+        };
+    };
+
+    binsFilter = [
+        "holochain"
+        "hc"
+        "kitsune-p2p-proxy"
+        "kitsune-p2p-tx2-proxy"
+    ];
+
+
+    lair = {
+        url = "https://github.com/holochain/lair";
+        rev = "lair_keystore_api-v0.2.0";
+        sha256 = "sha256-n7nZyZR0Q68Uff7bTSVFtSDLi21CNcyKibOBx55Gasg=";
+
+        binsFilter = [
+            "lair-keystore"
+        ];
+
+
+        cargoLock = {
+            outputHashes = {
+            };
+        };
+    };
+}
