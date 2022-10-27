8caac838c7b6eda4e0799b712a3a8f6749a6a43b
Holochain Release Automation <hrabot@holochain.org>
2022-10-27 14:50:15 +0000

    update nvfetcher sources
    the following keys were updated
    
    v0_0_169

--- nix/nvfetcher/_sources/generated.nix
+++ nix/nvfetcher/_sources/generated.nix
@@ -1200,6 +1200,24 @@
       };
     };
   };
+  holochain_revision_holochain-0_0_169 = {
+    pname = "holochain_revision_holochain-0_0_169";
+    version = "holochain-0.0.169";
+    src = fetchgit {
+      url = "https://github.com/holochain/holochain";
+      rev = "holochain-0.0.169";
+      fetchSubmodules = false;
+      deepClone = false;
+      leaveDotGit = false;
+      sha256 = "sha256-O5ieU54YH3M3Rl/0XT9YKomn1NRDTmypB3Nq2WuIjAA=";
+    };
+    cargoLock = {
+      lockFile = ./holochain_revision_holochain-0_0_169-holochain-0.0.169/Cargo.lock;
+      outputHashes = {
+        
+      };
+    };
+  };
   lair_revision_lair_keystore_api-v0_2_0 = {
     pname = "lair_revision_lair_keystore_api-v0_2_0";
     version = "lair_keystore_api-v0.2.0";
