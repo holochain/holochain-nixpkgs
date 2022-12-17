8d01153afa15f4863490cfc98f68293c1984d41f
Holochain Release Automation <hrabot@holochain.org>
2022-12-17 14:59:27 +0000

    update nvfetcher sources
    the following keys were updated
    
    develop

--- packages/holochain/versions/develop.nix
+++ packages/holochain/versions/develop.nix
@@ -4,8 +4,8 @@
 
 {
     url = "https://github.com/holochain/holochain";
-    rev = "6adca3a4ab0121754be3af019c75a5222c17779a";
-    sha256 = "sha256-Hn2Ldyak2+OA+OlaHmQ5Nj+e9XTtYwuBk2G7p1CVTsg=";
+    rev = "eb1e637c353190bfdba8b742c6b8bbb1b3f4a669";
+    sha256 = "sha256-H6kJeYP2DcMQcb5tBQpt/XtCqfr+P9XLgNMWzG1Nwaw=";
     cargoLock = {
         outputHashes = {
         };
