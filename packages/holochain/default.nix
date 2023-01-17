ef122907fe1e8c899ec1bacd69ee835b04293f83
DavHau <hsngrmpf+github@gmail.com>
2023-01-17 12:48:56 +0800

    chore(macos): add aarch64-darwin to supported platforms

--- packages/holochain/default.nix
+++ packages/holochain/default.nix
@@ -167,7 +167,12 @@ let
 
       doCheck = false;
 
-      meta.platforms = [ "aarch64-linux" "x86_64-linux" "x86_64-darwin" ];
+      meta.platforms = [
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-linux"
+        "x86_64-darwin"
+      ];
     });
 
   mkHolochainAllBinaries =
