5097b8db86c46c4264e994b64c98e0b7a054a407
Stefan Junker <mail@stefanjunker.de>
2023-01-18 10:30:05 -0600

    chore: fixup the develop version for testing

--- packages/holochain/versions/develop.nix
+++ packages/holochain/versions/develop.nix
@@ -3,35 +3,45 @@
 # For usage instructions please visit https://github.com/holochain/holochain-nixpkgs/#readme
 
 {
-    url = "https://github.com/holochain/holochain";
-    rev = "ca75e980f926c72d1d082239c8a7252a71254421";
-    sha256 = "sha256-2PCaKrE39fwdMNUTqbZMUCLjjkoq+u1bYCOrcdEMZ84=";
+  url = "https://github.com/holochain/holochain";
+  rev = "3b292847a74c9d91c20876ddad8ef6c9997d770f";
+  sha256 = "sha256-k/nKIhNHmhjhYwcRMuhKQ+ZpC64fYSY8FH1aCzhKlq8=";
+  cargoLock = { outputHashes = { }; };
+
+  binsFilter = [ "holochain" "hc" "kitsune-p2p-proxy" "kitsune-p2p-tx2-proxy" ];
+
+  lair = {
+    url = "https://github.com/holochain/lair";
+    rev = "lair_keystore_api-v0.2.3";
+    sha256 = "sha256-cqOr7iWzsNeomYQiiFggzG5Dr4X0ysnTkjtA8iwDLAQ=";
+
+    binsFilter = [ "lair-keystore" ];
+
+    cargoLock = { outputHashes = { }; };
+  };
+
+  scaffolding = {
+    url = "https://github.com/holochain/scaffolding";
+    rev = "holochain_scaffolding_cli-v0.0.6";
+    sha256 = "sha256-IlQ1OnsJP7T4Tc3JxoRuKKDQLlg11U9DzSAezO0pZ7c=";
+
+    binsFilter = [ "hc-scaffold" ];
+
+    cargoLock = { outputHashes = { }; };
+  };
+
+  launcher = {
+    url = "https://github.com/holochain/launcher";
+    rev = "holochain_cli_launch-0.0.5";
+    sha256 = "sha256-H53sxXHXifdrE0h0shQY/3DPI3eXAS74FdK7W/nj6pE=";
+
+    binsFilter = [ "hc-launch" ];
+
     cargoLock = {
-        outputHashes = {
-        };
+      outputHashes = {
+        "holochain_client-0.2.0" =
+          "sha256-zJGc2H+dGFz5/yd9ryG6q94qBhsLdrJBjuBahcRWtGE=";
+      };
     };
-
-    binsFilter = [
-        "holochain"
-        "hc"
-        "kitsune-p2p-proxy"
-        "kitsune-p2p-tx2-proxy"
-    ];
-
-
-    lair = {
-        url = "https://github.com/holochain/lair";
-        rev = "lair_keystore_api-v0.2.3";
-        sha256 = "sha256-cqOr7iWzsNeomYQiiFggzG5Dr4X0ysnTkjtA8iwDLAQ=";
-
-        binsFilter = [
-            "lair-keystore"
-        ];
-
-
-        cargoLock = {
-            outputHashes = {
-            };
-        };
-    };
+  };
 }
