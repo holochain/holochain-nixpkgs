f67365dd4d42adca1417a39ccd66e2a0ee91b092
Stefan Junker <mail@stefanjunker.de>
2023-01-21 11:43:29 +0100

    Revert "chore(nix): remove nvfetcher override"
    This reverts commit 85552de4dcdc85237dec6373dd038c66a13c53b6.

--- overlays/default.nix
+++ overlays/default.nix
@@ -5,6 +5,8 @@
 
   overrides = self: super: {
     toplevel = builtins.toString ./..;
+    nvfetcher =
+      (import self.localSources.nvfetcher.src).defaultPackage."${self.system}";
 
     pkgsPure = import self.localSources.nixpkgs.src { inherit (self) system; };
 
