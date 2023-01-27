15f37798c54ceab20c014fa603ddf814f93660f0
Holochain Release Automation <100725712+holochain-release-automation2@users.noreply.github.com>
2023-01-27 02:05:38 +0100

    update holochain-tags on develop (#1036)
    * update holochain tags
    
    packages/holochain/versions/update_config.toml:
    
    changed: true
    added (1): {
        "v0_1_0",
    }
    
    .github/workflows/build.yml:
    
    changed: true
    removed (1): {
        "v0_1_0-beta-rc_3",
    }
    added (1): {
        "v0_1_0",
    }
    
    * update nvfetcher sources
    
    the following keys were updated
    
    v0_1_0
    
    Co-authored-by: Holochain Release Automation <hrabot@holochain.org>

---
+++ packages/holochain/versions/v0_1_0.nix
@@ -0,0 +1,69 @@
+# This file was generated with the following command:
+# update-holochain-versions --nvfetcher-dir=nix/nvfetcher --output-file=packages/holochain/versions/v0_1_0.nix --bins-filter=holochain,hc,kitsune-p2p-proxy,kitsune-p2p-tx2-proxy --git-src=revision:holochain-0.1.0 --lair-version-req=~0.2
+# For usage instructions please visit https://github.com/holochain/holochain-nixpkgs/#readme
+
+{
+    url = "https://github.com/holochain/holochain";
+    rev = "holochain-0.1.0";
+    sha256 = "sha256-jSEWpZut7OgdBVPOpLoLPGAZT0+pnSngvc5oEsNy67M=";
+    cargoLock = {
+        outputHashes = {
+        };
+    };
+
+    binsFilter = [
+        "holochain"
+        "hc"
+        "kitsune-p2p-proxy"
+        "kitsune-p2p-tx2-proxy"
+    ];
+
+
+    lair = {
+        url = "https://github.com/holochain/lair";
+        rev = "lair_keystore_api-v0.2.3";
+        sha256 = "sha256-cqOr7iWzsNeomYQiiFggzG5Dr4X0ysnTkjtA8iwDLAQ=";
+
+        binsFilter = [
+            "lair-keystore"
+        ];
+
+
+        cargoLock = {
+            outputHashes = {
+            };
+        };
+    };
+
+    scaffolding = {
+        url = "https://github.com/holochain/scaffolding";
+        rev = "holochain_scaffolding_cli-v0.0.10";
+        sha256 = "sha256-fecZzSv84Z8LgynXqxIc3f4Kx8Y9wo5zwmHVz+Nw3fI=";
+
+        binsFilter = [
+            "hc-scaffold"
+        ];
+
+
+        cargoLock = {
+            outputHashes = {
+            };
+        };
+    };
+
+    launcher = {
+        url = "https://github.com/holochain/launcher";
+        rev = "holochain_cli_launch-0.0.8";
+        sha256 = "sha256-sn5C7pobUcBE+knt1wk6KTW/mWE7Pem6RVTdm6+F8uw=";
+
+        binsFilter = [
+            "hc-launch"
+        ];
+
+
+        cargoLock = {
+            outputHashes = {
+            };
+        };
+    };
+}
