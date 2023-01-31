e4687147a4dfe6373d775d2e473247ce12695b97
Holochain Release Automation <100725712+holochain-release-automation2@users.noreply.github.com>
2023-01-31 06:40:44 +0100

    update nvfetcher source: rust-overlay (#1048)
    Co-authored-by: Holochain Release Automation <hrabot@holochain.org>

--- nix/nvfetcher/_sources/generated.nix
+++ nix/nvfetcher/_sources/generated.nix
@@ -166,12 +166,12 @@
       fetchSubmodules = false;
       deepClone = false;
       leaveDotGit = false;
-      sha256 = "sha256-KQwVxqzP4/Edwhs8rmotN+jYcRZqQuVsEBVG68jgVjI=";
+      sha256 = "sha256-zLp8pEl3nLKh6SidAQprDmmbFvHUKpxUCZoJU4HJrBo=";
     };
     cargoLock = {
       lockFile = ./holochain_cli_launch_revision_holochain_cli_launch-0_0_6-holochain_cli_launch-0.0.6/Cargo.lock;
       outputHashes = {
-        "holochain_client-0.2.0" = "sha256-cRhj8LVE9e1tOpUeDIRwpVyov6yMlnkSDA44DqVY5U4=";
+        "holochain_client-0.2.0" = "sha256-NwvDNJ36h5k/6TsuFuOuejCIIyRHAMD/NrTwPsC0k4M=";
       };
     };
   };
@@ -1996,14 +1996,14 @@
   };
   rust-overlay = {
     pname = "rust-overlay";
-    version = "7093dfcd21df83026ee20426af80bf2072e2da92";
+    version = "48b1403150c3f5a9aeee8bc4c77c8926f29c6501";
     src = fetchgit {
       url = "https://github.com/oxalica/rust-overlay";
-      rev = "7093dfcd21df83026ee20426af80bf2072e2da92";
+      rev = "48b1403150c3f5a9aeee8bc4c77c8926f29c6501";
       fetchSubmodules = false;
       deepClone = false;
       leaveDotGit = false;
-      sha256 = "sha256-fd+BMct3RNqW4mESQlqCFnwCPEe+bHhkZIDVGBQn6GI=";
+      sha256 = "sha256-izOVjdIfdv0OzcfO9rXX0lfGkQn4tdJ0eNm3P3LYo/o=";
     };
   };
 }
