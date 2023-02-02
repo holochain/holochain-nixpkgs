4b1d7f18dedc97d745b7af366785242499d6454d
Stefan Junker <mail@stefanjunker.de>
2023-02-02 22:15:43 +0100

    update nvfetcher sources
    the following keys were updated
    
    v0_1_2

---
+++ packages/holochain/versions/v0_1_2.nix
@@ -0,0 +1,69 @@
+# This file was generated with the following command:
+# update-holochain-versions --nvfetcher-dir=nix/nvfetcher --output-file=packages/holochain/versions/v0_1_2.nix --bins-filter=holochain,hc,kitsune-p2p-proxy,kitsune-p2p-tx2-proxy --git-src=revision:holochain-0.1.2 --lair-version-req=~0.2
+# For usage instructions please visit https://github.com/holochain/holochain-nixpkgs/#readme
+
+{
+    url = "https://github.com/holochain/holochain";
+    rev = "holochain-0.1.2";
+    sha256 = "sha256-W4gin3S6Tu/OJSqi521E0Ds2sYNcDZyOV+pIKYOvsw8=";
+    cargoLock = {
+        outputHashes = {
+        };
+    };
+
+    binsFilter = [
+        "holochain"
+        "hc"
+        "kitsune-p2p-proxy"
+        "kitsune-p2p-tx2-proxy"
+    ];
+
+
+    lair = {
+        url = "https://github.com/holochain/lair";
+        rev = "lair_keystore_api-v0.2.3";
+        sha256 = "sha256-cqOr7iWzsNeomYQiiFggzG5Dr4X0ysnTkjtA8iwDLAQ=";
+
+        binsFilter = [
+            "lair-keystore"
+        ];
+
+
+        cargoLock = {
+            outputHashes = {
+            };
+        };
+    };
+
+    scaffolding = {
+        url = "https://github.com/holochain/scaffolding";
+        rev = "holochain_scaffolding_cli-v0.1.5";
+        sha256 = "sha256-roFeEEHyGpPdAsyFwKdDwPPtGuXNhbOqu9K8giOEXmI=";
+
+        binsFilter = [
+            "hc-scaffold"
+        ];
+
+
+        cargoLock = {
+            outputHashes = {
+            };
+        };
+    };
+
+    launcher = {
+        url = "https://github.com/holochain/launcher";
+        rev = "holochain_cli_launch-0.0.9";
+        sha256 = "sha256-FC/PT/9fvMvDVbk+S2gWXOAkp8hcEWU0hnaBi9cJVHE=";
+
+        binsFilter = [
+            "hc-launch"
+        ];
+
+
+        cargoLock = {
+            outputHashes = {
+            };
+        };
+    };
+}
